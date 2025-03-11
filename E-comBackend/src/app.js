0f0a5faa2d74bd4d59980c0132c4a2fda6b687de
Naveenmanral1 <123979484+Naveenmanral1@users.noreply.github.com>
2025-03-11 13:56:28 +0530

    cors

--- E-comBackend/src/app.js
+++ E-comBackend/src/app.js
@@ -8,13 +8,21 @@ import orderRouter from "./routes/order.routes.js"
 import wishlistRouter from "./routes/wishlist.routes.js"
 const app = express()
 
-console.log("CORS Origin:", process.env.CORS_ORIGIN);
+const allowedOrigins = process.env.CORS_ORIGIN.split(",")
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true,
-}));
-
+app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      credentials: true, 
+    })
+  );
+  
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"))
